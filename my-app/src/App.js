6b7d28820d75d37b2fc37b931d9083a06243934f
Nate LaBine <njlabine@gmail.com>
2022-07-07 20:38:33 -0400

    working header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
-
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+	return(
+  	<header className="App-header">
+    	<div>
+      	<h1>HERE IS MY HEADER</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
